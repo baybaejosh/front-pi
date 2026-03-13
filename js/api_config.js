a5a0606a761859ea2d2c7b02c563d896e0841936
JAntonio Godoy <jantonio.godoylopez@gmail.com>
2026-03-13 13:07:31 -0600

    v1.1 para prod agregado exept para ngrok

--- js/api_config.js
+++ js/api_config.js
@@ -2,7 +2,7 @@
 
 // URL base de tu compañero de Java (Ngrok)
 // const API_BASE_URL = "http://localhost:4000/api";
-const API_BASE_URL = "https://7f3c-2806-2f0-92a0-e8d3-00-d.ngrok-free.app/api";
+const API_BASE_URL = "https://9d74-2806-2f0-92a0-e8d3-00-d.ngrok-free.app/api";
 
 // const API_GO_URL = "http://localhost:5000/api";
 const API_GO_URL = "https://unphonnetically-unnegotiable-damon.ngrok-free.dev/api";
